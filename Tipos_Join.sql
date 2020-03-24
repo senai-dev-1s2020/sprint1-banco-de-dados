cc2c538e8f36d177e86de2a914dd69c4c967b799
Saulo <saulo@info.local>
2020-03-24 10:36:39 -0300

    Adiciona conteudo extra tipos de join

---
+++ Tipos_Join.sql
@@ -0,0 +1,60 @@
+-- Cria um banco de dados
+CREATE DATABASE Tipos_Join;
+GO
+
+-- Define o uso do banco de dados
+USE Tipos_Join;
+GO
+
+-- Cria as tabelas
+CREATE TABLE TiposUsuario(
+	IdTipoUsuario	INT PRIMARY KEY IDENTITY,
+	Titulo			VARCHAR (150) UNIQUE NOT NULL
+);
+GO
+
+CREATE TABLE Usuarios(
+	IdUsuario		INT PRIMARY KEY IDENTITY,
+	Nome			VARCHAR (150) NOT NULL,
+	TipoUsuario		INT FOREIGN KEY REFERENCES TiposUsuario (IdTipoUsuario)
+);
+GO
+
+-- Insere dois tipos de usuário
+INSERT INTO TiposUsuario (Titulo)
+VALUES					 ('Administrador'), ('Comum');
+GO
+
+-- Insere dois usuários
+INSERT INTO Usuarios (Nome, TipoUsuario)
+VALUES				 ('Usuário A', 1), ('Usuário B', 2);
+GO
+
+-- Insere um usuário sem definir o tipo de usuário
+INSERT INTO Usuarios (Nome)
+VALUES				 ('Usuário C');
+GO
+
+-- Lista todos os tipos de usuário
+SELECT IdTipoUsuario, Titulo FROM TiposUsuario;
+
+-- Lista todos os usuários
+SELECT IdUsuario, Nome, TipoUsuario FROM Usuarios;
+
+-- Lista todos os usuários mostrando as informações do tipo de usuário
+SELECT IdUsuario, Nome, Titulo FROM Usuarios
+-- JOIN TiposUsuario OU
+INNER JOIN TiposUsuario
+ON Usuarios.TipoUsuario = TiposUsuario.IdTipoUsuario;
+
+-- Lista todos os usuários mostrando as informações do tipo de usuário
+-- mesmo que um usuário não tenha um tipo definido
+SELECT IdUsuario, Nome, Titulo FROM Usuarios
+LEFT JOIN TiposUsuario
+ON Usuarios.TipoUsuario = TiposUsuario.IdTipoUsuario;
+
+-- Lista os usuários mostrando as informações do tipo de usuário
+-- mesmo que um tipo de usuário sem um usuário definido
+SELECT IdUsuario, Nome, Titulo FROM Usuarios
+RIGHT JOIN TiposUsuario
+ON Usuarios.TipoUsuario = TiposUsuario.IdTipoUsuario;
